a53fa2e74700641382364566c501fe34d639bbca
Karol Dzialowski <karlososhd@gmail.com>
2017-10-20 08:26:58 +0200

    Parse squad

--- Scrapper.py
+++ Scrapper.py
@@ -1,4 +1,11 @@
 import requests
+def player_clean(player):
+    change_time = 0
+    if player.find("(") != -1:
+        change_time = player[player.find("(")+1:player.find("'")]
+        change_time = int(change_time)
+        player = player[:player.find("(")]
+    return (player.strip(), change_time)
 import sqlite3
 from bs4 import BeautifulSoup
 
@@ -74,6 +81,49 @@ class Parser:
 
             self.matches.append((match, clubs[0], clubs[1], score_a, score_b))
             print(club_names[0] + " " + score_a + ":" + score_b + " " + club_names[1])
+            print(self.get_squads(soup, match))
+
+    def get_squads(self, page, match):
+        players = []
+        squad_grid = page.find('section', {'class': 'report-teams-players'})
+        squads = squad_grid.find_all('div', {'class': 'grid-24'})
+        for squad in squads:
+            club = squad.find('span').find('a')['href']
+            if (club == self.url_to_club_page):
+                # coach = club.find('span', {'class': 'coach-name'})
+                lists = squad.find_all('div', {'class': 'report-players-list'})
+                first_players = lists[0].find_all('a')
+                for player in first_players:
+                    player_name = player.find('div', {'class': 'player-name'}).text.strip()
+                    player_name, time_played = self.player_clean(player_name)
+                    if time_played == 0:
+                        time_played = 90;
+                    player_number = player.find('div', {'class': 'player-nr'}).text.strip()
+                    player_id = player['href']
+                    players.append((player_name, player_id, player_number, time_played, 1, club, match))
+
+                bench_players = lists[1].find_all('a')
+                for player in bench_players:
+                    player_name = player.find('div', {'class': 'player-name'}).text.strip()
+                    player_name, time_sub = self.player_clean(player_name)
+                    if time_sub > 0:
+                        time_played = 90 - int(time_sub)
+                        if time_played < 0:
+                            time_played = 1
+                    else:
+                        time_played = 0
+                    player_number = player.find('div', {'class': 'player-nr'}).text.strip()
+                    player_id = player['href']
+                    players.append((player_name, player_id, player_number, time_played, 0, club, match))
+        return players
+
+    def player_clean(self, player):
+        change_time = 0
+        if player.find("(") != -1:
+            change_time = player[player.find("(")+1:player.find("'")]
+            change_time = int(change_time)
+            player = player[:player.find("(")]
+        return (player.strip(), change_time)
 
     def get_match_page(self, match_link):
         result = requests.get(match_link)
